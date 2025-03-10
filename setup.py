c0482d57e2792fc3ed8b8739dcf90654c1a55211
Abhi12002 <mishraabinav30@gmail.com>
2025-03-10 10:19:30 -0400

    Project work started

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhinav Mishra",
+    author_email="mishraabinav30@gmail.com",
+    packages=find_packages()
+)
